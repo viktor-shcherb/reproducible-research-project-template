96c73e68d9b4a5a23597487e07f61eccb282b92c
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2025-07-27 14:47:52 +0200

    Remove orphans

--- sync_dependencies.sh
+++ sync_dependencies.sh
@@ -2,12 +2,12 @@
 set -e
 
 echo "Compiling dependencies inside Docker environment..."
-docker-compose run --rm deps
+docker-compose run --rm deps --remove-orphans
 
 echo "Rebuilding Docker Compose services..."
 docker-compose build
 
 echo "Restarting Docker Compose services..."
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 echo "✅    Dependencies synced and Docker Compose environment updated."
